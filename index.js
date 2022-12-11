5a28a8b7cd2e5850efd3fddd48ba450ca04d53fc
amin <amin.roosta@outlook.com>
2022-12-10 20:49:35 -0500

    Fix reading unit test inputs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const puppeteer       = require('puppeteer');
-const commandLineArgs = require('command-line-args')
-const fs              = require('fs');
+const puppeteer = require("puppeteer");
+const commandLineArgs = require("command-line-args");
+const fs = require("fs");
 const { join, resolve } = require("path");
 const readline = require("readline-sync");
 const execSync = require("child_process").execSync;
@@ -9,10 +9,9 @@ const execSync = require("child_process").execSync;
 // cs -c 1221                   # clone contest 1221
 // cs -s a                      # submit solution a
 const options = commandLineArgs([
-  { name: 'contest', alias: 'c', type: String },
-  { name: 'submit', alias: 's', type: String }
+  { name: "contest", alias: "c", type: String },
+  { name: "submit", alias: "s", type: String },
 ]);
-
 
 async function clone_contest(number) {
   const browser = await puppeteer.launch();
@@ -21,40 +20,49 @@ async function clone_contest(number) {
   console.warn(`openning ${url} ...`);
   await page.goto(url);
   const ids = await page.evaluate(() => {
-    const ids = document.querySelectorAll('table.problems tr td.id')
-    return [].map.call(ids, id => id.textContent.trim().toLowerCase());
+    const ids = document.querySelectorAll("table.problems tr td.id");
+    return [].map.call(ids, (id) => id.textContent.trim().toLowerCase());
   });
-  console.warn(`Found ${ids.length} problems : ${ids.join(', ')}`);
-  for(let id of ids) {
+  console.warn(`Found ${ids.length} problems : ${ids.join(", ")}`);
+  for (let id of ids) {
     const suburl = `${url}/problem/${id}`;
     console.warn(`openning ${suburl} ...`);
     await page.goto(suburl);
-    await page.waitForSelector('.sample-test');
+    await page.waitForSelector(".sample-test");
     const input_outputs = await page.evaluate(() => {
-        const inputs  = document.querySelectorAll('.sample-test .input pre');
-        const outputs = document.querySelectorAll('.sample-test .output pre');
-        return {
-            inputs : [].map.call(
-              inputs,
-              pre => [].map.call(
-                pre.querySelectorAll('div'),
-                div => div.textContent.trim()
-              ).join('\n')
-            ),
-            outputs: [].map.call(outputs, pre => pre.textContent.trim()),
-        };
+      const inputs = document.querySelectorAll(".sample-test .input pre");
+      const outputs = document.querySelectorAll(".sample-test .output pre");
+      return {
+        inputs: [].map.call(inputs, (pre) => {
+          const divs = pre.querySelectorAll("div");
+          if (divs.length) {
+            return [].map
+              .call(pre.querySelectorAll("div"), (div) =>
+                div.textContent.trim()
+              )
+              .join("\n");
+          } else {
+            return pre.textContent.trim();
+          }
+        }),
+        outputs: [].map.call(outputs, (pre) => pre.textContent.trim()),
+      };
     });
-    if (!fs.existsSync('./src')){ fs.mkdirSync('./src'); }
-    if (!fs.existsSync('./src/bin')){ fs.mkdirSync('./src/bin'); }
+    if (!fs.existsSync("./src")) {
+      fs.mkdirSync("./src");
+    }
+    if (!fs.existsSync("./src/bin")) {
+      fs.mkdirSync("./src/bin");
+    }
     write_question({
-        inputs : input_outputs.inputs,
-        outputs: input_outputs.outputs,
-        url    : suburl,
-        id     : id,
+      inputs: input_outputs.inputs,
+      outputs: input_outputs.outputs,
+      url: suburl,
+      id: id,
     });
-  };
+  }
 
-  console.warn('done');
+  console.warn("done");
   await browser.close();
 }
 
@@ -73,17 +81,20 @@ async function submit_problem(submit) {
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const url = 'https://codeforces.com/enter';
+  const url = "https://codeforces.com/enter";
   console.warn(`openning ${url} ...`);
   await page.goto(url);
-  await page.$eval('#handleOrEmail', (el, v) => (el.value = v), username);
-  await page.$eval('#password', (el, v) => (el.value = v), password);
+  await page.$eval("#handleOrEmail", (el, v) => (el.value = v), username);
+  await page.$eval("#password", (el, v) => (el.value = v), password);
   await page.click('input[type="submit"]');
   await page.waitForSelector(`a[href$="${username}"]`);
   fs.writeFileSync(cred_file, JSON.stringify({ username, password }));
 
   const file_path = resolve(submit);
-  const suburl = fs.readFileSync(file_path, "utf8").split('\n')[0].replace(/^\/\/\s*/, '');
+  const suburl = fs
+    .readFileSync(file_path, "utf8")
+    .split("\n")[0]
+    .replace(/^\/\/\s*/, "");
   console.warn(`openning ${suburl} ...`);
   await page.goto(suburl);
   await page.waitForSelector('input[name="sourceFile"]');
@@ -92,36 +103,39 @@ async function submit_problem(submit) {
   await input.uploadFile(file_path);
 
   console.warn(`submitting ${submit} ...`);
-  await page.click('form.submitForm input[type=submit]');
-  await page.waitForSelector('table tr[data-submission-id], .error');
+  await page.click("form.submitForm input[type=submit]");
+  await page.waitForSelector("table tr[data-submission-id], .error");
 
   const error = await page.evaluate(() => {
-    const error = document.querySelector('.error');
+    const error = document.querySelector(".error");
     return error && error.textContent;
   });
-  if(error) {
-      console.log(`\x1b[31m${error}`);
-      await browser.close();
-      return;
+  if (error) {
+    console.log(`\x1b[31m${error}`);
+    await browser.close();
+    return;
   }
 
-
-  while(1) {
+  while (1) {
     const data = await page.evaluate(() => {
-        const trs = Array.from(document.querySelectorAll('table tr[data-submission-id]'));
-        const data = {};
-        trs.slice(0,1).map(tr => {
-            const tds = Array.from(tr.querySelectorAll('td'));
-            const [id, when, _who, problem, lang, verdict, time, memory] = tds.map(td => td.textContent.trim());
-            data[id] = { when, problem, lang, verdict, time, memory };
-        });
-        return data;
+      const trs = Array.from(
+        document.querySelectorAll("table tr[data-submission-id]")
+      );
+      const data = {};
+      trs.slice(0, 1).map((tr) => {
+        const tds = Array.from(tr.querySelectorAll("td"));
+        const [id, when, _who, problem, lang, verdict, time, memory] = tds.map(
+          (td) => td.textContent.trim()
+        );
+        data[id] = { when, problem, lang, verdict, time, memory };
+      });
+      return data;
     });
     console.table(data);
-    const is_running = 
-          JSON.stringify(data).indexOf('queue') !== -1 ||
-          JSON.stringify(data).indexOf('Running') !== -1;
-    if(is_running) { 
+    const is_running =
+      JSON.stringify(data).indexOf("queue") !== -1 ||
+      JSON.stringify(data).indexOf("Running") !== -1;
+    if (is_running) {
       execSync("sleep 1");
       // await page.reload();
     } else {
@@ -131,11 +145,10 @@ async function submit_problem(submit) {
   }
 }
 
-
 (async () => {
   if (options.contest) {
     await clone_contest(options.contest);
-  } else if(options.submit) {
+  } else if (options.submit) {
     await submit_problem(options.submit);
   } else {
     console.warn(`EROOR, USEAGE ↓
@@ -145,10 +158,8 @@ async function submit_problem(submit) {
   }
 })();
 
-
-
-function write_question({inputs, outputs, url, id}) {
-let template = `// ${url}
+function write_question({ inputs, outputs, url, id }) {
+  let template = `// ${url}
 macro_rules! input{
     ($iter:expr) => {};
     ($iter:expr,) => {};
@@ -198,8 +209,8 @@ fn main() {
     run(&mut out, &s);
 }
 `;
- 
-for(let i = 0; i < inputs.length; ++i) {
+
+  for (let i = 0; i < inputs.length; ++i) {
     const input = inputs[i];
     const output = outputs[i];
     template += `
@@ -215,9 +226,9 @@ fn test_${i}() {
     let result = str::from_utf8(&vec).unwrap();
     assert_eq!(result.trim(), "${output}");
 }`;
-}
+  }
 
-    const file = `./src/bin/${options.contest}_${id}.rs`;
-    console.warn(`created file ${file}.`);
-    fs.writeFileSync(file, template);
-};
+  const file = `./src/bin/${options.contest}_${id}.rs`;
+  console.warn(`created file ${file}.`);
+  fs.writeFileSync(file, template);
+}
